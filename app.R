aeb869120892778764280e611b468b7a59326411
hafsah-usman <146280072+hafsah-usman@users.noreply.github.com>
2023-12-10 21:23:51 -0800

    Update app.R
    included all navbar info

--- app.R
+++ app.R
@@ -3,11 +3,11 @@ library(shiny)
 # Define UI
 ui <- fluidPage(
   navbarPage(
-    "Your Shiny App",
+    "Mental Health and COVID-19",
     tabPanel("Intro", source("intro.R")),
-    tabPanel("Story 1", source("story1.R")),
-    tabPanel("Story 2", source("story2.R")),
-    tabPanel("Story 3", source("story3.R")),
+    tabPanel("Story 1", source("interactivepage1.R")),
+    tabPanel("Story 2", source("interactivepage2.R")),
+    tabPanel("Story 3", source("interactivepage3.R")),
     tabPanel("Summary", source("summary.R"))
   )
 )
