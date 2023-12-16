b3917a32d9b53c354983f8f2575d01e277c6742a
anuujinch <149352977+anuujinch@users.noreply.github.com>
2023-12-15 20:55:41 -0800

    Update interactivepage3.R

--- interactivepage3.R
+++ interactivepage3.R
@@ -10,6 +10,7 @@ library(shiny)
 library(lubridate)
 library(ggplot2)
 library(tidyr)
+library(shinythemes)
 
 data <- read.csv("unified_and_cleaned_data.csv")
 
@@ -19,16 +20,17 @@ data <- filter(data, !str_detect(Indicator, "Symptoms of Anxiety Disorder or Dep
 data <- mutate(data, row_number = row_number() %% 2)
 data <- group_by(data, MonthYear, row_number)
 
-processed_data <- summarise(data,
-                            Depression = ifelse(row_number == 0, sum(Average_Percentage_per_Condition), NA),
-                            Anxiety = ifelse(row_number == 1, sum(Average_Percentage_per_Condition), NA))
-
-processed_data <- ungroup(processed_data)
+processed_data <- data %>%
+  group_by(MonthYear, row_number) %>%
+  summarise(Depression = ifelse(row_number == 0, sum(Average_Percentage_per_Condition), NA),
+            Anxiety = ifelse(row_number == 1, sum(Average_Percentage_per_Condition), NA),
+            .groups = "drop") %>%
+  ungroup()
 
 final_data <- left_join(data, processed_data, by = c("MonthYear", "row_number"))
 for (i in seq(1, nrow(data), by = 2)) {
   final_data$Depression[i] <- final_data$Depression[i + 1]
-
+  
 }
 
 final_data <- final_data[!is.na(final_data$Anxiety), ]
@@ -62,6 +64,12 @@ ui <- fluidPage(
   p(""),
   p("This graph shows the change in percentage of adults in the United States showing signs of Anxiety and Depression, as well as the average new cases"),
   p("for each month. Levels of anxiety and depression can be seeing overtime, as the number of cases change. "),
+  sliderInput("date_range", "Select Date Range:",
+              min = min(as.Date(paste0(data$MonthYear, "-01")), na.rm = TRUE),
+              max = max(as.Date(paste0(data$MonthYear, "-01")), na.rm = TRUE),
+              value = c(min(as.Date(paste0(data$MonthYear, "-01")), na.rm = TRUE),
+                        max(as.Date(paste0(data$MonthYear, "-01")), na.rm = TRUE)),
+              step = 1),
   p(""),
   p(""),
   p(""),
@@ -71,18 +79,22 @@ ui <- fluidPage(
 
 # -------------------------------- SERVER ------------------------------------ #
 server <- function(input, output) {
-
-  long_data <- gather(final_data, key = "Legend", value = "Value", -MonthYear)
-
-  output$plot <- renderPlot({
-    ggplot(long_data, aes(x = MonthYear, y = Value, fill = Legend)) +
-      geom_bar(stat = "identity", position = "dodge", width = 0.7) +
-      ylab("Values") +
-      xlab("Date") +
-      ggtitle("Depression, Anxiety, and New COVID-19 Cases Over Time") +
-      theme_minimal()
-  })
+  final_data$MonthYear <- as.Date(paste0(final_data$MonthYear, "-01"))
   
-  }
+  observe({
+    filtered_data <- subset(final_data, MonthYear >= input$date_range[1] & MonthYear <= input$date_range[2])
+    
+    long_data <- gather(filtered_data, key = "Legend", value = "Value", -MonthYear)
+    
+    output$plot <- renderPlot({
+      ggplot(long_data, aes(x = MonthYear, y = Value, fill = Legend)) +
+        geom_bar(stat = "identity", position = "dodge", width = 0.7) +
+        ylab("Values") +
+        xlab("Date") +
+        ggtitle("Depression, Anxiety, and New COVID-19 Cases Over Time") +
+        theme_minimal()
+    })
+  })
+}
 
 shinyApp(ui = ui, server = server)
